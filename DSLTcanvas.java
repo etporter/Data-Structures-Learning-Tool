9613431ba6e613f2769940496dd270ff7f8fbe22
Ethan <ethan@duane-shimmel-6.wireless.um.maine.edu>
2014-12-02 11:14:20 -0500

    update all class files

---
+++ DSLTcanvas.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class DSLTcanvas extends Canvas {
+
+	public DSLTcanvas()
+	{
+		super();
+	}
+	
+}
---
+++ DSLTtree.java
@@ -0,0 +1,13 @@
+
+public interface DSLTtree {
+	
+	/**
+	 * @return the integers stored in the tree in level order
+	 */
+	public int[] nodes();
+	
+	/**
+	 * @return an array of
+	 */
+	public boolean[] reds();
+}
