da448339d2bb2b0ea9909e0eb97ba555dde30d67
etporter <ethan.porter@maine.edu>
2014-12-03 11:38:48 -0500

    Create TreeDisplay.java

---
+++ src/TreeDisplay.java
@@ -0,0 +1,24 @@
+
+public class TreeDisplay {
+	
+	DSLTtree myTree;
+	
+	/**
+	 * @param t: the tree to be displayed
+	 */
+	public TreeDisplay(DSLTtree t)
+	{
+		myTree = t;
+	}
+	
+	private void drawCircle(int x,int y,int radius,boolean red)
+	{
+		
+	}
+	
+	public void drawTree()
+	{
+		
+	}
+
+}
