7477854bba5f7f781780b6b4353dec3c48324505
etporter <ethan.porter@maine.edu>
2014-12-03 11:37:58 -0500

    Create DSLTtree.java

---
+++ src/DSLTtree.java
@@ -0,0 +1,12 @@
+public interface DSLTtree {
+	
+	/**
+	 * @return the integers stored in the tree in level order
+	 */
+	public int[] nodes();
+	
+	/**
+	 * @return an array of
+	 */
+	public boolean[] reds();
+}
