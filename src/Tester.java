a6a6d14f239f9fcaf1790d9852577e4477117333
seraphinarose <seraphina.orsini@maine.edu>
2014-12-09 09:53:37 -0500

    Create Tester.java

---
+++ src/Tester.java
@@ -0,0 +1,13 @@
+
+public class Tester {
+
+		  public static void main (String[] args) {
+			  
+		    //Creates new Window, and sets to visible
+		    Window thisWindow = new Window();
+		    thisWindow.setSize(1500,800);
+		    thisWindow.setVisible(true);
+		  }
+		
+
+}
